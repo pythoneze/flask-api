ffd36ccaea81c8268843be8e1e4324ad07da595e
pythoneze <pythoneze@gmail.com>
2025-02-22 17:51:33 -0300

    Create data model

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
 @app.route("/")
 def home():
     return '<h1>Fask REST API App</h1>'
